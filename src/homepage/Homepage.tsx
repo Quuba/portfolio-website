4783cab142a5d8e34bc27315e578ed3ea20a18f7
Quuba <66736611+Quuba@users.noreply.github.com>
2022-04-19 21:17:25 +0200

    added placeholder content

---
+++ src/homepage/Homepage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './homepage.css';
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Website under construction</h1>
+            <h3>Come again later</h3>
+            <div id="img-container">
+                <div className="box">
+                    <img src="./images/website_under_construction.jpg" alt="Strona w budowie"/>
+                </div>
+            </div>
+            <a href="https://jakubcendrowski.pl/quizappka">quiz appka</a>
+        </div>
+    );
+};
+
+export default Homepage;
